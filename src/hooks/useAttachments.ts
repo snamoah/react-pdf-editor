81402cd8709768d6d5e22c84c60c38f419b49ca2
Samuel Amoah <sa.am@programmer.net>
2020-09-02 20:26:55 +0200

    refactor(useAttachment): use useReducer instead of useState

--- src/hooks/useAttachments.ts
+++ src/hooks/useAttachments.ts
@@ -1,89 +1,154 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
 
-export const useAttachments = () => {
-  const [pageIndex, setPageIndex] = useState(-1);
-  const [allPageAttachments, setAllPageAttachments] = useState<Attachments[]>(
-    []
-  );
-  const [pageAttachments, setPageAttachments] = useState<Attachments>([]);
+enum ActionType {
+  RESET = 'RESET',
+  ADD_ATTACHMENT = 'ADD_ATTACHMENT',
+  REMOVE_ATTACHMENT = 'REMOVE_ATTACHMENT',
+  UPDATE_ATTACHMENT = 'UPDATE_ATTACHMENT',
+  UPDATE_PAGE_INDEX = 'UPDATE_PAGE_INDEX',
+}
 
-  useEffect(() => {
-    const pageAttachmentToSet = allPageAttachments[pageIndex];
+interface State {
+  pageIndex: number;
+  allPageAttachments: Attachments[];
+  pageAttachments: Attachments;
+}
 
-    setPageAttachments(pageAttachmentToSet);
-  }, [pageIndex, allPageAttachments]);
-
-  const add = (newAttachment: Attachment) => {
-    if (pageIndex === -1) {
-      return;
+type Action =
+  | { type: ActionType.UPDATE_PAGE_INDEX; pageIndex: number }
+  | { type: ActionType.ADD_ATTACHMENT; attachment: Attachment }
+  | { type: ActionType.REMOVE_ATTACHMENT; attachmentIndex: number }
+  | {
+      type: ActionType.UPDATE_ATTACHMENT;
+      attachmentIndex: number;
+      attachment: Partial<Attachment>;
     }
+  | { type: ActionType.RESET; numberOfPages: number };
 
-    setAllPageAttachments(
-      allPageAttachments.map((attachments, index) =>
-        pageIndex === index ? [...attachments, newAttachment] : attachments
-      )
-    );
-  };
+const initialState: State = {
+  pageIndex: -1,
+  allPageAttachments: [],
+  pageAttachments: [],
+};
 
-  const remove = useCallback(
-    (attachmentIndex: number) => {
+const reducer = (state: State, action: Action) => {
+  const { pageIndex, allPageAttachments, pageAttachments } = state;
+
+  switch (action.type) {
+    case ActionType.ADD_ATTACHMENT: {
       if (pageIndex === -1) {
-        return;
+        return state;
       }
 
-      setAllPageAttachments(
-        allPageAttachments.map((otherPageAttachments, index) =>
+      const newAllPageAttachmentsAdd = allPageAttachments.map(
+        (attachments, index) =>
+          pageIndex === index
+            ? [...attachments, action.attachment]
+            : attachments
+      );
+
+      return {
+        ...state,
+        allPageAttachments: newAllPageAttachmentsAdd,
+        pageAttachments: newAllPageAttachmentsAdd[pageIndex],
+      };
+    }
+    case ActionType.REMOVE_ATTACHMENT: {
+      if (pageIndex === -1) {
+        return state;
+      }
+
+      const newAllPageAttachmentsRemove = allPageAttachments.map(
+        (otherPageAttachments, index) =>
           pageIndex === index
             ? pageAttachments.filter(
-                (_, _attachmentIndex) => _attachmentIndex !== attachmentIndex
+                (_, _attachmentIndex) =>
+                  _attachmentIndex !== action.attachmentIndex
               )
             : otherPageAttachments
-        )
       );
-    },
-    [setAllPageAttachments]
-  );
 
-  const update = useCallback(
-    (attachmentIndex: number, attachment: Partial<Attachment>) => {
+      return {
+        ...state,
+        allPageAttachments: newAllPageAttachmentsRemove,
+        pageAttachments: newAllPageAttachmentsRemove[pageIndex],
+      };
+    }
+    case ActionType.UPDATE_ATTACHMENT: {
       if (pageIndex === -1) {
-        return;
+        return state;
       }
 
-      setAllPageAttachments(
-        allPageAttachments.map((otherPageAttachments, index) =>
+      const newAllPageAttachmentsUpdate = allPageAttachments.map(
+        (otherPageAttachments, index) =>
           pageIndex === index
             ? pageAttachments.map((oldAttachment, _attachmentIndex) =>
-                _attachmentIndex === attachmentIndex
-                  ? { ...oldAttachment, ...attachment }
+                _attachmentIndex === action.attachmentIndex
+                  ? { ...oldAttachment, ...action.attachment }
                   : oldAttachment
               )
             : otherPageAttachments
-        )
       );
-    },
-    [setAllPageAttachments]
-  );
 
-  const reset = useCallback(
-    (numberOfPages: number) => {
-      setAllPageAttachments(Array(numberOfPages).fill([]));
-    },
-    [setAllPageAttachments]
-  );
+      return {
+        ...state,
+        allPageAttachments: newAllPageAttachmentsUpdate,
+        pageAttachments: newAllPageAttachmentsUpdate[pageIndex],
+      };
+    }
+    case ActionType.UPDATE_PAGE_INDEX: {
+      return {
+        ...state,
+        pageIndex: action.pageIndex,
+        pageAttachments: allPageAttachments[action.pageIndex],
+      };
+    }
+    case ActionType.RESET: {
+      return {
+        pageIndex: 0,
+        pageAttachments: [],
+        allPageAttachments: Array(action.numberOfPages).fill([]),
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
-  const setPageIndexHandler = useCallback((index) => setPageIndex(index), [
-    setPageIndex,
-  ]);
+export const useAttachments = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { allPageAttachments, pageAttachments } = state;
+
+  const add = (newAttachment: Attachment) =>
+    dispatch({ type: ActionType.ADD_ATTACHMENT, attachment: newAttachment });
+
+  const remove = (attachmentIndex: number) =>
+    dispatch({ type: ActionType.REMOVE_ATTACHMENT, attachmentIndex });
+
+  const update = (attachmentIndex: number, attachment: Partial<Attachment>) =>
+    dispatch({
+      type: ActionType.UPDATE_ATTACHMENT,
+      attachmentIndex,
+      attachment,
+    });
+
+  const reset = (numberOfPages: number) =>
+    dispatch({ type: ActionType.RESET, numberOfPages });
+
+  const setPageIndex = useCallback(
+    (index: number) =>
+      dispatch({ type: ActionType.UPDATE_PAGE_INDEX, pageIndex: index }),
+    [dispatch]
+  );
 
   return {
     add,
     reset,
     remove,
     update,
-    setPageIndex: setPageIndexHandler,
+    setPageIndex,
     pageAttachments,
     allPageAttachments,
-    setAllPageAttachments,
   };
 };
